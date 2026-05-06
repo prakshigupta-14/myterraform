151329d3ff662064bde3f26c029a264ca25096dd
prakshigupta-14 <prakshi.gupta@hcltech.com>
2026-05-06 14:58:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
+variable "project_id" {
+  description = "The GCP project ID where resources will be created"
+  type        = string
+}
 
+variable "region" {
+  description = "The GCP region to deploy resources in"
+  type        = string
+  default     = "us-central1"
+}
