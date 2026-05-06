68bfff3a98170421f4bef9a719c7fc8e738ca861
prakshigupta-14 <prakshi.gupta@hcltech.com>
2026-05-06 14:58:29 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,16 @@
 
+output "instance_name" {
+  description = "Name of the Compute Engine instance"
+  value       = google_compute_instance.vm.name
+}
+
+output "instance_zone" {
+  description = "Zone where the instance is deployed"
+  value       = google_compute_instance.vm.zone
+}
+
+output "instance_external_ip" {
+  description = "External IP address of the instance"
+  value       = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+}
+
