940b59f6c9aa2d518a368251e9d2975a7d120598
prakshigupta-14 <prakshi.gupta@hcltech.com>
2026-05-06 14:54:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "tf-harness-demo"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
